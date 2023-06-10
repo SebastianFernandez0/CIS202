1475b2220ebd77f00bf93214290dcf75eda7986b
SebastianFernandez0 <129228881+SebastianFernandez0@users.noreply.github.com>
2023-06-10 05:57:05 +0000

    initial commit.

--- final-project/main.cpp
+++ final-project/main.cpp
@@ -1,10 +1,69 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <string>
-#include"mountain.h"
+#include <vector>
+
 
 using namespace std;
+
+class Mountain {
+
+private:
+    string mounName;
+    string country;
+    double elevation;
+
+public:
+    // ----------Constructor------------
+    Mountain(const string& Name, const string& Country, double Elevation){
+        this->mounName = Name;
+        this->country = Country;
+        this->elevation = Elevation;
+    }
+
+    // ------------Getters-------------
+    string getName() const {
+        return mounName;
+    }
+
+    string getCo() const {
+        return country;
+    } 
+
+    double getElv() const {
+        return elevation;
+    }
+
+    // -----------Setters--------------
+    void setNa(const string& Name){
+        this->mounName = Name;
+    }
+
+    void setCo(const string& Country){
+        this->country = Country;
+    }
+
+    void setEl(double Elevation){
+        this->elevation = Elevation;
+    }
+
+    double toMeters() const {
+        return elevation / 3.2808;
+    }
+};
+
+Mountain findMin(const vector<Mountain>& mountains) {
+
+    Mountain smallest = mountains[0];
+
+    for (const Mountain& mountain : mountains) {
+
+        if (mountain.getElv() < smallest.getElv()) {
+            smallest = mountain;
+        }
+    }
+    return smallest;
+}
 
 int main() {
 // Mountain Info
@@ -20,18 +79,18 @@ int main() {
     info.push_back(Mountain("Zugspitze", "Switzerland", 9719));
 
     // Title
-    cout << left << setw(30) << "Mountain" << setw(25) << "Country" << setw(18) << "Elevation (ft)" << "Elevation (m)" << endl;
+    cout << left << setw(31) << "Mountain" << setw(27) << "Country" << setw(19) << "Elevation (ft)" << "Elevation (m)" << endl;
     cout << "--------------------------------------------------------------------------------------" << endl;
 
     // Table
     for (const Mountain& mountain : info) {
-        cout << left << setw(30) << mountain.getNa() << setw(25) << mountain.getCo() << setw(18) << mountain.getElv() << mountain.toMeters() << endl;
+        cout << left << setw(30) << mountain.getName() << setw(25) << mountain.getCo() << setw(18) << mountain.getElv() << mountain.toMeters() << endl;
     }
 
     Mountain min = findMin(info);
 
     cout << endl;
-    cout<< min.getNa() << " is the smallest mountain in the list with a Elevation of " << min.getElv() << " ft." << endl;
+    cout<< min.getName() << " is the smallest mountain with an Elevation of " << min.getElv() << " ft." << endl;
 
     return 0;
 }
